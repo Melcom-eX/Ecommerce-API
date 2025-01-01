744459dadcd0dd94617d7df41f04ead8d6c44400
Churchill <churchillrimamskep@gmail.com>
2025-01-01 19:50:15 +0100

    feat(review): added the review component

--- src/components/review/review.validation.ts
+++ src/components/review/review.validation.ts
@@ -4,5 +4,5 @@ export const reviewSchema = Joi.object({
   userId: Joi.string().required(),
   productId: Joi.string().required(),
   rating: Joi.number().min(1).max(5).required(),
-  comment: Joi.string().optional(),
+  comment: Joi.string().required(),
 });
