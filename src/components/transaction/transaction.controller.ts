272609662b2a781cdf57e350d5f1d0ac4b8c2992
Churchill <churchillrimamskep@gmail.com>
2025-01-19 19:35:26 +0100

    done with the transaction component now to write test

--- src/components/transaction/transaction.controller.ts
+++ src/components/transaction/transaction.controller.ts
@@ -4,7 +4,7 @@ import { TransactionDTO } from "./transaction.validation";
 import { transactionService } from "./transaction.service";
 
 class TransactionController {
-  async createTransaction(req: Request, res: Response) {
+  async createTransaction(req: Request, res: Response): Promise<Response> {
     try {
       const userId = req.user?.id;
       if (!userId) {
@@ -19,14 +19,31 @@ class TransactionController {
         req.body as TransactionDTO
       );
 
-      return res.status(201).json({
-        status: true,
-        message: "transaction succesful",
-        data: transaction,
-      });
+      return res.status(transaction.statusCode).send(transaction);
     } catch (error) {
       console.error("Delete user error:", error);
       return res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
+  async getAllTransactions(req: Request, res: Response): Promise<Response> {
+    try {
+      const transactions = await transactionService.getAllTransactions();
+      return res.status(transactions.statusCode).send(transactions);
+    } catch (error) {
+      console.error("Delete user error:", error);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
+  async getTransaction(req: Request, res: Response): Promise<Response> {
+    try {
+      const { id } = req.params;
+      const transaction = await transactionService.getTransaction(id);
+      return res.status(transaction.statusCode).send(transaction);
+    } catch (error) {
+      console.error("Error creating transaction:", error);
+      throw error;
     }
   }
 }
--- src/components/transaction/transaction.service.ts
+++ src/components/transaction/transaction.service.ts
@@ -2,6 +2,8 @@ import httpStatus from "http-status";
 import { transactionRepository } from "./transaction.repository";
 import { TransactionResponse } from "./transaction.response";
 import { TransactionDTO } from "./transaction.validation";
+import { Transaction } from "@prisma/client";
+import { Errors } from "../../error/error";
 
 class TransactionService {
   async createTransaction(
@@ -25,7 +27,7 @@ class TransactionService {
         transactionData
       );
       return {
-        statusCode: httpStatus.OK,
+        statusCode: httpStatus.CREATED,
         error: false,
         status: "success",
         message: "transaction created succesfully",
@@ -34,6 +36,46 @@ class TransactionService {
     } catch (error) {
       console.error("Error creating transaction:", error);
       throw error;
+    }
+  }
+  async getAllTransactions(): Promise<{
+    status: string;
+    error: boolean;
+    statusCode: number;
+    message: string;
+    data?: Transaction[];
+  }> {
+    try {
+      const data = await transactionRepository.findAll();
+      return {
+        statusCode: httpStatus.OK,
+        error: false,
+        status: "success",
+        message: "transactions retireved succesfully",
+        data: data,
+      };
+    } catch (error) {
+      console.error("Error creating transaction:", error);
+      throw error;
+    }
+  }
+
+  async getTransaction(
+    id: string
+  ): Promise<TransactionResponse | typeof Errors.doesNotExist> {
+    try {
+      const transaction = await transactionRepository.findById(id);
+      if (!transaction) return Errors.doesNotExist;
+      return {
+        statusCode: httpStatus.OK,
+        error: false,
+        status: "success",
+        message: "transaction retireved succesfully",
+        data: transaction,
+      };
+    } catch (error) {
+      console.error(error);
+      return Errors.defaultError;
     }
   }
 }
